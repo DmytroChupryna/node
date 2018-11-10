769ae81e05cf4ac1e54d75bee77d70662d78ea33
Dchup <dchup@softserveinc.com>
2018-11-10 18:57:33 +0200

    expess start

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello Node');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("Use 1 !")
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Use 2 !")
+    res.send('<h1>Hello from Express!</h2>');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
