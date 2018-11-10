94f113b78746ca1d4dd5a68cde477145d09b0504
Dchup <dchup@softserveinc.com>
2018-11-10 22:46:41 +0200

    next

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const http = require('http');
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const rootDir = require('./util/path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 app.use((req, res, next) => {
-    console.log("Use 1 !")
-    next();
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 });
 
-app.use((req, res, next) => {
-    console.log("Use 2 !")
-    res.send('<h1>Hello from Express!</h2>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
